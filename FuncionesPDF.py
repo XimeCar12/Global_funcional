d48d75de0212767244727054f7e74cb56c62ecf5
XimeCar12 <xcarpiojuarez@gmail.com>
2023-08-11 10:21:01 -0600

    Proyecto final

--- FuncionesPDF.py
+++ FuncionesPDF.py
@@ -16,16 +16,15 @@ def generarPDF(pos):
     generarQR(nombreQR,"hola desde la funcion")
     c= canvas.Canvas(nombreArchivo)
 
-    #now = datetime.now()
-    #fecha = now.strftime("%Y_%m_%d %H_%M_%S")
 
+    c.setFont('Helvetica-Bold', 26)
     c.drawString(200,780,"MUSA nail's")
 
     c.setFont('Helvetica-Bold', 16)
     c.drawString(40,750, "Cliente:")
     c.drawString(180,750, listaClientes[pos])
     c.drawString(40,730, "Fecha:")
-    #c.drawString(180,730, fecha )
+    c.drawString(180,730,fecha_actual.strftime('%d/%m/%Y %H:%M:%S'))
 
     c.drawString(250,660, "Productos")
 
@@ -33,25 +32,29 @@ def generarPDF(pos):
     y = A4[1]- 190
     c.line(x, y, x + 880, y)
 
-    c.drawString(50,630, "Producto: ")
-    c.drawString(50,600,listaProductos[pos])
+    c.drawString(30,630, "Producto: ")
+    c.drawString(30,600,listaProductos[pos])
     c.drawString(180,630, "Servicio: ")
-    c.drawString(180,600, listaServicios[pos])
-    c.drawString(180,630, "Combo: ")
-    c.drawString(180,600, listaCombos[pos])
-    c.drawString(310,630, "Precio: ")
-    c.drawString(490,630, "Total")
+    c.drawString(120,600, listaServicios[pos])
+    c.drawString(400,630, "Combo: ")
+    c.drawString(350,600, listaCombos[pos])
+
+
 
     x = 0
     y = A4[1] -220
     c.line(x,y,x+800,y)
 
-   #xInicial =200
-    #yInicial = 700
-    #c.drawImage(nombreQR,200,400,100,100)
-    #for i in range(len(listaNombre)):
-        #yInicial = yInicial -20
-        #c.drawString(xInicial,yInicial,listaEdades[i])
-        #c.drawString(xInicial+120,yInicial,listaNombre[i])
+    # QR
+    informacion = f"Gracias por tu compra, Si sigues con nosotros puedes llevarte un premio e incluso obtener un premio"
+    img = qrcode.make(informacion)
+    nombreImagen = ruta + "miQR.png"
+    img.save(nombreImagen)
+
+
+
+    c.drawImage(nombreQR,220,100,100,100)
+    c.drawString(100,50, "Escanea el QR para asi poder obtener algun premio")
+
     c.save()
     print("-------Reporte generado--------")
